809a09a0c75465ca6ff33c3b85754622b6b1a2ae
Daniel Beckwith <djbsnx@gmail.com>
2021-09-16 17:37:54 -0400

    Remove TODO

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #![warn(rust_2018_idioms, clippy::all)]
 #![deny(clippy::correctness)]
 
-// TODO: make sure to read the existing config and merge it with the new one
-// this way stateful or unknown props are preserved
-
 use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
