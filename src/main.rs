9c7c2e8cba31dc04d099b6309a21a916fd5ae05c
Daniel Beckwith <djbsnx@gmail.com>
2021-10-10 19:33:30 -0400

    Rotate log dir

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![deny(clippy::correctness)]
 
 use anyhow::{Context, Result};
-use std::fs;
+use std::{fs, path::PathBuf};
 use structopt::StructOpt;
 use xfce_config::{Applier, ClearPath, XfceConfig, XfceConfigPatch};
 
@@ -20,11 +20,11 @@ fn main() -> Result<()> {
     let dry_run = !args.apply;
     let clear_paths = args.clear_paths.unwrap_or_default();
 
-    // TODO: unique log subdir for each run?
-    let log_dir = dirs2::data_local_dir()
-        .context("could not get data local dir")?
-        .join("xfce-config");
-    fs::create_dir_all(&log_dir).context("error creating log dir")?;
+    let log_dir = rotating_log_dir(
+        dirs2::data_local_dir()
+            .context("could not get data local dir")?
+            .join("xfce-config"),
+    )?;
 
     let config_dir = dirs2::config_dir().context("could not get config dir")?;
     let xfce4_config_dir = config_dir.join("xfce4");
@@ -63,4 +63,29 @@ fn main() -> Result<()> {
     .context("error applying config")?;
 
     Ok(())
+}
+
+fn rotating_log_dir(dir: PathBuf) -> Result<PathBuf> {
+    let log_dir =
+        dir.join(chrono::Local::now().format("%Y-%m-%d-%H-%M-%S").to_string());
+    fs::create_dir_all(&log_dir).context("error creating log dir")?;
+    let mut log_dirs = fs::read_dir(dir)
+        .context("error reading log dir")?
+        .filter_map(|entry| {
+            entry
+                .context("error reading log dir entry")
+                .map(|entry| {
+                    let path = entry.path();
+                    path.is_dir().then(|| path)
+                })
+                .transpose()
+        })
+        .collect::<Result<Vec<_>>>()?;
+    log_dirs.sort();
+    // remove all but the last 10
+    for expired_log_dir in log_dirs.into_iter().rev().skip(10) {
+        fs::remove_dir_all(expired_log_dir)
+            .context("error removing old log dir")?;
+    }
+    Ok(log_dir)
 }
